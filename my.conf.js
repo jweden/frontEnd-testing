d10e4e7ab7ee09eebb2e640b2353b0c212b0c4f0
Jason Weden <jweden@constantcontact.com>
2013-10-13 11:08:12 -0400

    update to karma 0.10

--- my.conf.js
+++ my.conf.js
@@ -1,79 +1,72 @@
 // Karma configuration
-// Generated on Tue Jul 02 2013 11:40:24 GMT-0400 (Eastern Daylight Time)
-// base path, that will be used to resolve files and exclude
-basePath = '';
+// Generated on Sun Oct 13 2013 10:17:52 GMT-0400 (EDT)
 
-frameworks: ['jasmine'];
+module.exports = function(config) {
+  config.set({
 
-// list of files / patterns to load in the browser
-files = [
-   JASMINE,
-   JASMINE_ADAPTER,
-    {pattern: 'src/test/jasmine/lib/jquery-1.10.2.min.js', watched: false, served: true, included: true},
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
+
+
+    // frameworks to use
+    frameworks: ['jasmine'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+	{pattern: 'src/test/jasmine/lib/jquery-1.10.2.min.js', watched: false, served: true, included: true},
     {pattern: 'src/test/jasmine/lib/jasmine-jquery.js', watched: false, served: true, included: true},
     {pattern: 'src/main/webapp/myScript.js', watched: true, served: true, included: true},
     {pattern: 'src/test/jasmine/firstTest.js', watched: true, served: true, included: true}
-];
+      
+    ],
 
 
-proxies = {
+    // list of files to exclude
+    exclude: [
+      
+    ],
 
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['Chrome'],
+
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
 };
-
-// list of files to exclude
-exclude = [
-  
-];
-
-
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['progress'];
-
-
-// web server port
-port = 9876;
-
-
-// cli runner port
-runnerPort = 9100;
-
-
-// enable / disable colors in the output (reporters and logs)
-colors = true;
-
-
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-//logLevel = karma.LOG_INFO;
-
-
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
-
-
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome', 'IE'];
-
-
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
-
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = true;
-
-plugins: [
-    'karma-jasmine',
-    'karma-chrome-launcher',
-    'karma-firefox-launcher',
-    'karma-junit-reporter'
-];
